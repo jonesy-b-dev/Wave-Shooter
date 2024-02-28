f0cb2d4bee39450ba78da2f3dfeeb730b57531ea
Jonas de Bruin <59418915+jonesy-b-dev@users.noreply.github.com>
2024-02-28 11:00:50 +0100

    Moved lot of files around, added documentation and start of interactable

---
+++ Assets/Player/Scripts/CameraLook.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLook : MonoBehaviour
+{
+    [SerializeField] private InputManager inputManager;
+    [SerializeField] private float mouseSensitivity = 25f;
+    [SerializeField] private Transform body;
+
+    private float xRotation = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = inputManager.player_Mappings.Camera.MouseX.ReadValue<float>() * mouseSensitivity * Time.deltaTime;
+        Debug.Log("Mouse x: " + mouseX);
+
+        float mouseY = inputManager.player_Mappings.Camera.MouseY.ReadValue<float>() * mouseSensitivity * Time.deltaTime;
+        Debug.Log("Mouse y: " + mouseY);
+        xRotation -= mouseY;
+        xRotation = Math.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
+        body.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ Assets/Player/Scripts/PlayerMovement.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public InputManager inputManager;
+    public Rigidbody rb;
+
+    [SerializeField] private float speed = 10;
+    [SerializeField] private float runSpeed = 15;
+    [SerializeField] private float jumpForce = 200;
+
+    private bool isGrounded;
+
+    private void Start()
+    {
+        // Call jump function when jump button is pressed
+        inputManager.player_Mappings.Movement.Jump.started += _ => Jump();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Read for the input
+        float forward = inputManager.player_Mappings.Movement.Forward.ReadValue<float>(); 
+        float right = inputManager.player_Mappings.Movement.Right.ReadValue<float>(); 
+        
+        // Setup the move vector
+        Vector3 move = transform.right * right + transform.forward * forward;
+        
+        // Sprinting 
+        move *= inputManager.player_Mappings.Movement.Run.ReadValue<float>() == 0 ? speed : runSpeed;
+        
+        // Crouching
+        transform.localScale = new Vector3(1, inputManager.player_Mappings.Movement.Crouch.ReadValue<float>() == 0 ? 1f : 0.7f, 1);
+
+        // Move the player
+        rb.velocity = new Vector3(move.x, rb.velocity.y, move.z);
+    }
+
+
+    private void OnCollisionEnter(Collision other)
+    {
+        // Check if player is colliding with the ground tag
+        if(other.transform.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision other)
+    {
+        if (other.transform.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
+
+    void Jump()
+    {
+        if(isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce);
+        }
+    }
+}
