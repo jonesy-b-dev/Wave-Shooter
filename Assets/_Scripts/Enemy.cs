c58551f63c58db8a0fbf8af76ee23f760dc7503a
Jonas de Bruin <59418915+jonesy-b-dev@users.noreply.github.com>
2024-03-06 09:56:53 +0100

    Actually destroy enemy on hit

--- Assets/_Scripts/Enemy.cs
+++ Assets/_Scripts/Enemy.cs
@@ -31,7 +31,7 @@ public class Enemy : MonoBehaviour, IEnemy
     void IEnemy.Death()
     {
         Debug.Log("Dead");
-
-        renderer.enabled = false;
+     
+        Destroy(gameObject);
     }
 }
