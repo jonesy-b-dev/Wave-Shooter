c584ac86b294968a8d6ade0afa13c102f9b1a93a
Jonas de Bruin <59418915+jonesy-b-dev@users.noreply.github.com>
2024-03-07 11:00:51 +0100

    Simple enemy Spawner

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -1,15 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+// Serializable:
+    [SerializeField] private GameObject enemy;
+    [SerializeField] private int[] waveEnemyCount; 
+
+// Public:
     public static GameManager instance;
     public float playerHealth = 100;
+
+    private int currentWave;
+
     
     private void Awake()
     {
        if (instance != null && instance != this) Destroy(this);
        else instance = this;
+    }
+
+    private void Start()
+    {
+        currentWave = 0;
+        for (int i = 0; i < waveEnemyCount[currentWave]; i++)
+        {
+            Instantiate(enemy);
+        }
     }
 }
