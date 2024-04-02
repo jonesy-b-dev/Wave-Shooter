55527156ef9f88d2d6cfae9200bd9a2f4f69bba4
Jonas de Bruin <jonasdebruin05@gmail.com>
2024-04-02 15:47:52 +0200

    Attempt to fix options not working

--- Assets/_Scripts/OptionsHandler.cs
+++ Assets/_Scripts/OptionsHandler.cs
@@ -12,12 +12,17 @@ public class OptionsHandler : MonoBehaviour
     // Start is called before the first frame update
     void Awake()
     {
-        if (instance != null && instance != this) Destroy(this);
+        if (instance != null && instance != this){
+            Destroy(this);
         else instance = this;
     }
 
     public void SetSensitivity(Slider slider)
     {
         sensitivityMultiplier = slider.value;
+    }
+    private void Update()
+    {
+        Debug.Log(sensitivityMultiplier);
     }
 }
