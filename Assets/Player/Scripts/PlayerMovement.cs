8a0bb5417bd10a05197720e281c951f16fb6b5e7
Jonas de Bruin <jonasdebruin05@gmail.com>
2024-04-07 20:05:17 +0200

    Added footstep audio

--- Assets/Player/Scripts/PlayerMovement.cs
+++ Assets/Player/Scripts/PlayerMovement.cs
@@ -9,14 +9,18 @@ public class PlayerMovement : MonoBehaviour
 //Serializable:
     [Header("Components")]
     [SerializeField] private InputManager inputManager;
+    [SerializeField] private AudioManager audioManager;
     [SerializeField] private Rigidbody rb;
     [SerializeField] private Transform groundCheckTransform;
-
+    [Space(5)]
     [Header("Player Settings")]
     [SerializeField] private float speed = 10f;
     [SerializeField] private float runSpeed = 15f;
     [SerializeField] private float jumpForce = 200f;
     [SerializeField] private float groundCheckDistance = 0.1f;
+    [Space(5)]
+    [Header("Other")]
+    [SerializeField] private AudioClip[] footsteps;
 
 //private:
     private Vector3 move;
@@ -24,6 +28,7 @@ public class PlayerMovement : MonoBehaviour
     private float right;
     private bool isColliding;
     private bool isGrounded;
+    private float stepDelta;
 
     private void Start()
     {
@@ -45,12 +50,25 @@ public class PlayerMovement : MonoBehaviour
         move = transform.right * right + transform.forward * forward;
         // Sprinting 
         move *= inputManager.player_Mappings.Movement.Run.ReadValue<float>() == 0 ? speed : runSpeed;
-        
+
         // Crouching
         transform.localScale = new Vector3(1, inputManager.player_Mappings.Movement.Crouch.ReadValue<float>() == 0 ? 1f : 0.7f, 1);
 
         // Move the player
         rb.velocity = new Vector3(move.x, rb.velocity.y, move.z);
+        
+        if(move != new Vector3(0, 0, 0) && isGrounded)
+        {
+            stepDelta += inputManager.player_Mappings.Movement.Run.ReadValue<float>() == 0 ? Time.deltaTime : Time.deltaTime * 2;
+        }
+
+        if(stepDelta > 0.5)
+        {
+            audioManager.PlayAudio(footsteps[UnityEngine.Random.Range(0, footsteps.Length)], transform.position);
+            stepDelta = 0;
+
+        }
+    
     }
 
     private void CheckGrounded()
